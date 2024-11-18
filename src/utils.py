d1b814cdb833c62c3bacc0b89e52cced09dd5b06
Yashwant <yashwantr.bsc22@rvu.edu.in>
2024-11-18 18:40:56 +0530

    Data transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import dill
+import os
+import pickle
+
 import pymongo
 from pymongo import MongoClient
 from src.exception import CustomException
@@ -23,4 +29,17 @@ def get_database_connection(uri: str, database_name: str):
         return db
     except Exception as e:
         logging.error("Error connecting to MongoDB.")
+        raise CustomException(e, sys)
+    
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
         raise CustomException(e, sys)
